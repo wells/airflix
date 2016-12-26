a46be6581715453a602ebd123b556e26122a7b0f
Brian Wells <wells@users.noreply.github.com>
2016-12-26 13:31:59 -0600

    Update NotFound component to vue v2.

--- resources/assets/js/components/NotFound.vue
+++ resources/assets/js/components/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="not-found">
-  <h1><i class="material-icons">&#xE001;</i> Page Not Found</h1>
+  <h1>
+  	<i class="material-icons">&#xE001;</i> Page Not Found
+  </h1>
 </div>
 </template>
 

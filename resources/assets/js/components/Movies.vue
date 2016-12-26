68ee2704bd9790b3cb6ddfdc8b2e9b2c338b3a1d
Brian Wells <wells@users.noreply.github.com>
2016-12-26 13:31:44 -0600

    Update Movies component to vue v2.

--- resources/assets/js/components/Movies.vue
+++ resources/assets/js/components/Movies.vue
@@ -1,28 +1,45 @@
 <template>
-<div class="movies" v-infinite-scroll="loadMore()" infinite-scroll-disabled="loading" infinite-scroll-distance="200">
-
-  <spinner v-show="$loadingRouteData" transition="loading"></spinner>
+<div class="movies">
+  <transition name="loading">
+    <spinner v-show="loadingRouteData"></spinner>
+  </transition>
 
   <!-- search -->
   <div class="search">
     <div class="fields">
-      <input class="form-control" placeholder="Keywords" :value="keywords" @input="filterMovieKeywords | debounce 250">
+      <input class="form-control" 
+          placeholder="Keywords" 
+          :value="keywords" 
+          @input="filterMovieKeywords" />
 
       <div class="dropdown order">
-        <select class="form-control" :value="order" @change="filterMovieOrder">
-          <option v-for="option in orders" :value="option.id">{{ option.name }}</option>
+        <select class="form-control" 
+            :value="selectedOrder" 
+            @change="filterMovieOrder">
+          <option v-for="option in orders" 
+              :value="option.id">
+            {{ option.name }}
+          </option>
         </select>
       </div>
 
       <div class="dropdown genres">
-        <select class="form-control" :value="selectedGenre" @change="filterMovieGenres">
+        <select class="form-control" 
+            :value="selectedGenre" 
+            @change="filterMovieGenres">
           <option value="">All Genres</option>
-          <option v-for="option in genres | filterByHasMovies | orderBy 'attributes.name'" :value="option.id">{{ option.attributes.name }}</option>
+          <option v-for="option in genres" 
+              :value="option.id">
+            {{ option.attributes.name }}
+          </option>
         </select>
       </div>
     </div>
     <div class="clear">
-      <a class="button" @click.prevent="filterReset">Clear</a>
+      <a class="button" 
+          @click.prevent="filterReset">
+        Clear
+      </a>
     </div>
   </div>
 
@@ -30,96 +47,176 @@
   <div class="results">
     <h1>Movies</h1>
     <div class="cards">
-      <movie-card v-for="movie in movies | filterBy keywords in 'attributes.title' | filterByGenre selectedGenre | orderBy column direction" track-by="id" :movie="movie">
+      <movie-card v-for="movie in movies" 
+          :key="movie.id" 
+          :movie="movie">
       </movie-card>
     </div>
   </div>
+
+  <mugen-scroll :handler="loadMore" 
+      :should-handle="!loading">
+  </mugen-scroll>
 </div>
 </template>
 
 <script>
 import MovieCard from './MovieCard.vue'
+import MugenScroll from 'vue-mugen-scroll'
 import Spinner from './Spinner.vue'
-import { getMovies } from '../vuex/actions/movies'
-import {
-  clearFilters,
-  clearGenresFilter, 
-  filterGenres, 
-  filterKeywords, 
-  filterOrder 
-} from '../vuex/actions/filters'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Movies',
+
   components: { 
     MovieCard,
+    MugenScroll,
     Spinner
   },
-  ready: function () {
-    this.$watch('links.next', function () {
-      // Automatically load paginated results
-      setTimeout(() => {
-        if(this.links.next && !this.loading) {
-          this.getMovies(this.links.next)
+
+  created: function () {
+    this.loadingRoute()
+    this.clearGenresFilter()
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': function () {
+      this.loadingRoute()
+      this.fetchData()
+    },
+
+    'links': function () {
+      let self = this
+
+      clearTimeout(self.loadingTimeout)
+
+      self.loadingTimeout = setTimeout(function () {
+        if(self.links && self.links.next && !self.loading) {
+          self.fetchData(self.links.next)
         }
-      }, 1000);
-    }, { deep: true })
-  },
-  vuex: {
-    getters: {
-      column: ({ filters }) => filters.attributes.movies,
-      direction: ({ filters }) => filters.direction,
-      genres: ({ genres }) => genres.all,
-      keywords: ({ filters }) => filters.keywords,
-      links: ({ movies }) => movies.links,
-      loading: ({ movies }) => movies.loading,
-      movies: ({ movies }) => movies.all,
-      order: ({ filters }) => filters.order,
-      orders: ({ filters }) => filters.orders,
-      query: ({ filters }) => filters.queryMovies,
-      selectedGenre: ({ filters }) => filters.genre
-    },
-    actions: {
-      clearFilters,
-      clearGenresFilter,
-      getMovies,
-      filterGenres,
-      filterKeywords,
-      filterOrder
+      }, 1000)
     }
   },
-  route: {
-    data: function (transition) {
-      this.clearGenresFilter()
-      this.getMovies(this.url, transition)
-    }
-  },
+
   methods: {
-    loadMore: function () {
-      if (this.links.next && !this.loading && !this._inactive) {
-        this.getMovies(this.links.next)
+    ...mapActions([
+      'clearFilters',
+      'clearGenresFilter',
+      'getMovies',
+      'filterGenres',
+      'filterKeywords',
+      'filterOrder',
+      'loadingRoute'
+    ]),
+
+    fetchData: function (payloadUrl) {
+      if(! payloadUrl) {
+        payloadUrl = '/api/movies' + this.$store.state.filters.queryMovies
       }
+
+      let payload = {
+        url: payloadUrl
+      }
+
+      this.getMovies(payload)
     },
+
     filterMovieGenres: function (event) {
       this.filterGenres(event)
-      this.getMovies(this.url)
+      this.fetchData()
     },
-    filterMovieKeywords: function (event) {
-      this.filterKeywords(event)
-      this.getMovies(this.url)
-    },
+
+    filterMovieKeywords: _.debounce(
+      function (event) {
+        this.filterKeywords(event)
+        this.fetchData()
+      }, 
+      250
+    ),
+
     filterMovieOrder: function (event) {
       this.filterOrder(event)
-      this.getMovies(this.url)
+      this.fetchData()
     },
+
     filterReset: function () {
       this.clearFilters()
-      this.getMovies(this.url)
-    }
+      this.fetchData()
+    },
+
+    loadMore: _.throttle(
+      function () {
+        if (this.links && this.links.next && !this.loading) {
+          this.fetchData(this.links.next)
+        }
+      },
+      500
+    )
   },
+
   computed: {
-    url: function() {
-      return '/api/movies' + this.query
+    genres: function () {
+      return _.chain(this.$store.state.genres.all)
+        .sortBy('attributes.name')
+        // Has movies
+        .filter(function (item) { 
+          return item.attributes.total_movies > 0 
+        })
+        .value()
+    },
+
+    keywords: function () {
+      return this.$store.state.filters.keywords
+    },
+
+    links: function () {
+      return this.$store.state.movies.links
+    },
+
+    loading: function () {
+      return this.$store.state.movies.loading
+    },
+
+    loadingRouteData: function () {
+      return this.$store.state.interfaces.loadingRouteData
+    },
+
+    movies: function () {
+      let state = this.$store.state
+
+      let keywords = state.filters.keywords
+      let selectedGenre = state.filters.selectedGenre
+      let column = state.filters.attributes.movies
+      let direction = state.filters.direction
+
+      return _.chain(state.movies.all)
+        // Filter by keywords
+        .filter(function (item) {
+          let searchRegex = new RegExp(keywords, 'i')
+          return searchRegex.test(item.attributes.title)
+        })
+        // Filter by genre
+        .filter(function (item) { 
+          let record = item.relationships.genres.data.find(g => g.id === selectedGenre)
+          return selectedGenre == '' || record != null
+        })
+        // Order by column and direction
+        .orderBy([column], [direction])
+        .value()
+    },
+
+    orders: function () {
+      return this.$store.state.filters.orders
+    },
+
+    selectedGenre: function () {
+      return this.$store.state.filters.selectedGenre
+    },
+
+    selectedOrder: function () {
+      return this.$store.state.filters.selectedOrder
     }
   }
 }

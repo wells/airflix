e380bb56f71f827fecfdaa59bced356eb9ecad92
Brian Wells <wells@users.noreply.github.com>
2016-12-26 13:32:48 -0600

    Update Settings component to vue v2.

--- resources/assets/js/components/Settings.vue
+++ resources/assets/js/components/Settings.vue
@@ -1,8 +1,10 @@
 <template>
 <div class="settings">
-  <spinner v-show="$loadingRouteData" transition="loading"></spinner>
+  <transition name="loading">
+    <spinner v-show="loadingRouteData"></spinner>
+  </transition>
 
-  <div v-if="!$loadingRouteData">
+  <div v-if="!loadingRouteData">
     <h1>Settings</h1>
 
     <h2>
@@ -10,10 +12,10 @@
       Refresh
     </h2>
 
-    <a class="button" @click.prevent="refreshNew">
+    <a class="button" @click.prevent="refreshNewFolders">
       <i class="material-icons">&#xE2CC;</i> Refresh New
     </a>
-    <a class="button" @click.prevent="refreshAll">
+    <a class="button" @click.prevent="refreshAllFolders">
       <i class="material-icons">&#xE617;</i> Refresh All
     </a>
 
@@ -22,10 +24,10 @@
       History
     </h2>
 
-    <a class="button" @click.prevent="clearToday">
+    <a class="button" @click.prevent="clearHistoryToday">
       <i class="material-icons">&#xE8DF;</i> Clear Today
     </a>
-    <a class="button" @click.prevent="clearAll">
+    <a class="button" @click.prevent="clearHistory">
       <i class="material-icons">&#xE92B;</i> Clear All
     </a>
 
@@ -33,14 +35,26 @@
       <i class="material-icons">&#xE2C8;</i>
       Folders
     </h2>
+
     <p>
       Setting a folder path will refresh the database with 
       movies and/or shows found in that location.
     </p>
+
     <label>Movies Folder</label>
-    <input class="form-control" placeholder="Movies Folder (i.e. /movies)" :value="settings.attributes.movies_folder" @input="setMoviesFolder">
+
+    <input class="form-control" 
+        placeholder="Movies Folder (i.e. /movies)" 
+        :value="settings.attributes.movies_folder" 
+        @input="setMoviesFolder" />
+
     <label>Shows Folder</label>
-    <input class="form-control" placeholder="Shows Folder (i.e. /shows)" :value="settings.attributes.shows_folder" @input="setShowsFolder">
+
+    <input class="form-control" 
+        placeholder="Shows Folder (i.e. /shows)" 
+        :value="settings.attributes.shows_folder" 
+        @input="setShowsFolder" />
+
     <a class="button" @click.prevent="patchFolders">
       <i class="material-icons">&#xE2C7;</i> Update Folders
     </a>
@@ -49,50 +63,100 @@
 </template>
 
 <script>
-import {
-  clearAll,
-  clearToday,
-  getSettings,
-  patchSettings,
-  refreshAll,
-  refreshNew,
-  setMoviesFolder,
-  setShowsFolder
-} from '../vuex/actions/settings'
 import Spinner from './Spinner.vue'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Settings',
+
   components: {
     Spinner 
   },
-  vuex: {
-    getters: {
-      settings: ({ settings }) => settings.current
-    },
-    actions: {
-      clearAll,
-      clearToday,
-      getSettings,
-      patchSettings,
-      refreshAll,
-      refreshNew,
-      setMoviesFolder,
-      setShowsFolder
+
+  created: function () {
+    this.loadingRoute()
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': function () {
+      this.loadingRoute()
+      this.fetchData()
     }
   },
-  route: {
-    data (transition) {
-      this.getSettings(transition)
-    }
-  },
+
   methods: {
-    patchFolders: function () {
-      const data = {
-        'data': this.settings,
+    ...mapActions([
+      'clearAll',
+      'clearToday',
+      'getSettings',
+      'patchSettings',
+      'refreshAll',
+      'refreshNew',
+      'setMoviesFolder',
+      'setShowsFolder',
+      'loadingRoute'
+    ]),
+
+    clearHistory: function () {
+      let payload = {
+        url: '/api/settings/history?all'
       }
 
-      this.patchSettings(data)
+      this.clearAll(payload)
+    },
+
+    clearHistoryToday: function () {
+      let payload = {
+        url: '/api/settings/history'
+      }
+
+      this.clearToday(payload)
+    },
+
+    fetchData: function () {
+      let payload = {
+        url: '/api/settings'
+      }
+
+      this.getSettings(payload)
+    },
+
+    patchFolders: function () {
+      let payload = {
+        url: '/api/settings',
+        json: {
+          data: this.settings
+        }
+      }
+
+      this.patchSettings(payload)
+    },
+
+    refreshAllFolders: function () {
+      let payload = {
+        url: '/api/settings/folders?all'
+      }
+
+      this.refreshAll(payload)
+    },
+
+    refreshNewFolders: function () {
+      let payload = {
+        url: '/api/settings/folders'
+      }
+
+      this.refreshNew(payload)
+    }
+  },
+
+  computed: {
+    loadingRouteData: function () {
+      return this.$store.state.interfaces.loadingRouteData
+    },
+
+    settings: function ()  {
+      return this.$store.state.settings.current
     }
   }
 }
